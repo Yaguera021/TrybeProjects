d82ca8996f0c3e2c86caaf9ff170e07658daa580
Seu nome <yago_bohmcruz@hotmail.com>
2024-04-18 16:27:31 -0300

    BUg

--- OnlineShop/src/App.tsx
+++ OnlineShop/src/App.tsx
@@ -1,12 +1,12 @@
 import Header from './components/Header';
-// import Hero from './components/Hero';
+import Hero from './components/Hero';
 // import Products from './components/Products';
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Hero /> */}
+      <Hero />
       {/* <Products /> */}
     </>
   );
--- OnlineShop/src/components/Hero.tsx
+++ OnlineShop/src/components/Hero.tsx
@@ -1,7 +1,47 @@
 import './styles/Hero.css';
+import { useState } from 'react';
+import image1 from '../images/Online Store.png';
 
 function Hero() {
-  return <h2>Hero</h2>;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const images = [image1];
+
+  const nextImage = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex(
+      (currentImageIndex - 1 + images.length) % images.length,
+    );
+  };
+
+  return (
+    <section className='hero'>
+      <div className='hero__content'>
+        <h1 className='hero__title'>Welcome to our store</h1>
+        <p className='hero__description'>
+          Shop for the best products at the best prices
+        </p>
+        <div>
+          <img src={image1} alt='carousel' className='hero__image' />
+          <button
+            onClick={prevImage}
+            className='hero__button hero__button--prev'
+          >
+            Previous
+          </button>
+          <button
+            onClick={nextImage}
+            className='hero__button hero__button--next'
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
